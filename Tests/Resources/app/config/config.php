a8ce4ded757165bd9d3e0aa3003c0629c0f9867c
Lukas Kahwe Smith <smith@pooteeweet.org>
2014-10-12 06:16:00 -0400

    fix test setup, added php 5.6

--- Tests/Resources/app/config/config.php
+++ Tests/Resources/app/config/config.php
@@ -1,5 +1,7 @@
 <?php
 
+$container->setParameter('cmf_testing.bundle_fqn', 'Symfony\Cmf\Bundle\SearchBundle');
+
 $loader->import(CMF_TEST_CONFIG_DIR . '/default.php');
 $loader->import(CMF_TEST_CONFIG_DIR . '/phpcr_odm.php');
 
